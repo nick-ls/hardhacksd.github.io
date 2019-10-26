fd038358adf87a69cab4cc4735067cba9b5900b8
kehulse <kehulse@ucsd.com>
2019-10-26 14:59:39 -0700

    Hello World

---
+++ assets/js/main.js
@@ -0,0 +1,174 @@
+/*
+	Overflow by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#page-wrapper'),
+		$banner = $('#header'),
+		$header = $('#super'),
+		settings = {
+
+			// Parallax background effect?
+				parallax: true,
+
+			// Parallax factor (lower = more intense, higher = less intense).
+				parallaxFactor: 10
+
+		};
+
+	// Breakpoints.
+		breakpoints({
+			wide:    [ '1081px',  '1680px' ],
+			normal:  [ '841px',   '1080px' ],
+			narrow:  [ '737px',   '840px'  ],
+			mobile:  [ null,      '736px'  ]
+		});
+
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-scroll');
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('.scrolly-middle').scrolly({
+			speed: 1000,
+			anchor: 'middle'
+		});
+
+		$('.scrolly').scrolly({
+			speed: 1000,
+			offset: function() { return (breakpoints.active('<=mobile') ? 70 : 190); }
+		});
+
+	// Parallax background.
+
+		// Disable parallax on IE/Edge (smooth scrolling is jerky), and on mobile platforms (= better performance).
+			if (browser.name == 'ie'
+			||	browser.name == 'edge'
+			||	browser.mobile)
+				settings.parallax = false;
+
+		if (settings.parallax) {
+
+			var $dummy = $(), $bg;
+
+			$window
+				.on('scroll.overflow_parallax', function() {
+
+					// Adjust background position.
+					// $bg.css('background-position', 'center ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+
+				})
+				.on('resize.overflow_parallax', function() {
+
+					// If we're in a situation where we need to temporarily disable parallax, do so.
+						if (breakpoints.active('<=narrow')) {
+
+							$body.css('background-position', '');
+							$bg = $dummy;
+
+						}
+
+					// Otherwise, continue as normal.
+						else
+							$bg = $body;
+
+					// Trigger scroll handler.
+						$window.triggerHandler('scroll.overflow_parallax');
+
+				})
+				.trigger('resize.overflow_parallax'); 
+
+		}
+
+	// Poptrox.
+		$('.gallery').poptrox({
+			useBodyOverflow: false,
+			usePopupEasyClose: false,
+			overlayColor: '#0a1919',
+			overlayOpacity: 0.75,
+			usePopupDefaultStyling: false,
+			usePopupCaption: true,
+			popupLoaderText: '',
+			windowMargin: 10,
+			usePopupNav: true
+		});
+
+			// Menu.
+			$('#Smenu')
+			.append('<a href="#Smenu" class="close"></a>')
+			.appendTo($body)
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'right',
+				target: $body,
+				visibleClass: 'is-menu-visible'
+			});
+
+	// Header.
+		if ($banner.length > 0
+		&&	$header.hasClass('alt')) {
+
+			$window.on('resize', function() { $window.trigger('scroll'); });
+
+			$banner.scrollex({
+				bottom:		$header.outerHeight() + 1,
+				terminate:	function() { $header.removeClass('alt'); },
+				enter:		function() { $header.addClass('alt'); },
+				leave:		function() { $header.removeClass('alt'); }
+			});
+
+		}
+
+})(jQuery);
+
+/**
+ * The code for the countdown follows
+ */
+var deadline = 'January 17 2020 6:00 PM PST';
+
+function getTimeRemaining(endtime){
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor( (t/1000) % 60 );
+	var minutes = Math.floor( (t/1000/60) % 60 );
+	var hours = Math.floor( (t/(1000*60*60)) % 24 );
+	var days = Math.floor( t/(1000*60*60*24) );
+	return {
+		'total': t,
+		'days': days,
+		'hours': hours,
+		'minutes': minutes,
+		'seconds': seconds
+	};
+}
+
+function initializeClock(id, endtime){
+	var clock = document.getElementById(id);
+	var timeinterval = setInterval(function(){
+		var t = getTimeRemaining(endtime);
+		clock.innerHTML = t.days + ' days ' +
+                          t.hours + ' hours ' +
+						  t.minutes + ' minutes ' +
+						  t.seconds + ' seconds'
+		if(t.total <= 0) {
+			clearInterval(timeinterval);
+		}
+	},1000);
+}
+
+initializeClock('clockdiv', deadline);
