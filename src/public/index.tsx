82c2db33acfc23e2904099cd39dc69c37c183373
Nicholas264 <22565232+Nicholas264@users.noreply.github.com>
2022-03-03 12:31:35 -0800

    Add WIP version

---
+++ src/public/index.tsx
@@ -0,0 +1,29 @@
+import * as ReactDom from "react-dom";
+import * as React from "react";
+import TopBar from "./modules/TopBar";
+import HorizontalSection from "./modules/HorizontalSection";
+
+const LINKS = ["About", "FAQ", "Sponsors"];
+
+interface MainProps {}
+interface MainState {}
+
+class Main extends React.Component<MainProps, MainState> {
+	constructor(props: MainProps) {
+		super(props);
+		this.state = {};
+	}
+	public render() {
+		return <>
+			<img className="logo" src="img/hardhack.png"></img>
+			<TopBar links={LINKS}></TopBar>
+			<HorizontalSection title="About" id="about"></HorizontalSection>
+			<HorizontalSection title="FAQ" id="faq"></HorizontalSection>
+			<HorizontalSection title="Sponsors" id="sponsors"></HorizontalSection>
+		</>;
+	}
+}
+
+ReactDom.render(<Main></Main>, document.getElementById("root"));
+
+export default {};
